dd0b117cae0b13a69bcd191062dbad6bdb04396b
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-05-10 19:31:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,75 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const config = require('config'); // for db connection
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//configs 
+const dbConfig = config.get('Customer.dbConfig');
+//db.connect(dbConfig, ...);
 
-var app = express();
+// connection maken met db in config 
+//let dbConfig = config.get('../Customer.dbConfig'); // link naar config file
+
+//mongoose.connect(dbConfig, {useNewUrlParser: true});
+
+// routers opladen 
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let messagesRouter = require('./routes/api/v1/messages');
+
+
+
+mongoose.connect('mongodb://localhost:27017/messages', {
+  useNewUrlParser: true
+});
+// let connection = 'mongodb://localhost:27017/messageLab5';
+
+
+//////////
+// test mongoose connection
+//mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+  console.log("we're connected!");
+});
+//////////
+const app = express();
+//////////
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// aan bepaalde routes, een router functie toekennen
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1/messages', messagesRouter); // ??????
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.messages = err.messages;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
