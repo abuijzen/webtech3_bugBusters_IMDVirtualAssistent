7600508cd3566ccaa19ed3ee1c658ecb1ee1afcc
Angelique Buijzen <angeliquebuijzen@hotmail.com>
2019-05-12 14:03:02 +0200

    browsersync

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+
+const {
+    src,
+    dest,
+    parallel
+}= require('gulp');
+
+const browserSync = require('browser-sync').create();
+
+
+function doBrowserSync(){
+  
+    browserSync.init({
+        server:{
+            baseDir:"./views/signup.pug" 
+        },
+      });   
+ 
+}
+module.exports.default = parallel(doBrowserSync);
+
+
